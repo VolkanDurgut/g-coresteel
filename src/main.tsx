2fa34c8efabb5484aa4fd2c4c8b2053f3b814992
Volkan Durgut <volkandurgut.tr@gmail.com>
2025-06-24 17:53:03 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,17 @@
-// 'Suspense' i react'tan import ediyoruz
 import { StrictMode, Suspense } from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom'; // Burayı ekliyoruz
 import App from './App.tsx';
 import './index.css';
 import './i18n';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    {/* App bileşenini Suspense ile sarmalıyoruz */}
     <Suspense fallback="Yükleniyor...">
-      <App />
+      {/* BrowserRouter'u en dış sarmalayıcı yapıyoruz */}
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Suspense>
   </StrictMode>
 );
